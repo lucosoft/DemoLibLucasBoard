01b7f5f0f00ab579816a64564ec980eb496de8b8
MGUERRERO\Lucas Guerrero <manuellucasguerrero@yahoo.com.ar>
2018-04-19 15:45:54 -0300

    revision inicial

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -llib_lucas_board -llpc_board_nxp_lpcxpresso_1769 -llpc_chip_175x_6x
+
