ec844f91a5720b5a6f23d9e1712cf54006bf0f6c
LucasBF <lguerrero@baufest.com>
2019-05-24 17:08:07 -0300

    New feature ADC. Refactor please

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llib_lucas_board -llpc_board_nxp_lpcxpresso_1769 -llpc_chip_175x_6x
-
